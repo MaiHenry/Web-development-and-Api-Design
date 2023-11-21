4ce41d1cf6bcf977485c4495c1eb902b4e7900db
NXTDTA <Henry@nextdata.no>
2023-11-21 09:38:15 +0100

    ready to deploy

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
-import { Application } from "./Application";
+/* import { Application } from "./Application"; */
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(<Application />);
+/* root.render(<Application />); */
+root.render(<h1>My React Application</h1>);
