049933c21918d60ef45bbd74ad875677c99b4168
NXTDTA <Henry@nextdata.no>
2023-11-21 09:32:01 +0100

    initial client init

---
+++ client/index.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import { Application } from "./Application";
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<Application />);
